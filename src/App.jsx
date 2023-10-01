11072f6d6e3344c085cef25e7a127798c1be068a
nicojedo tp final <nicolasjedlinski@gmail.com>
2023-10-01 12:30:26 -0300

    agregando react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Componenten from "./componentes/Componenten"
+
+const App = () => {
+
+  return (
+    <>
+      <div className="titulo">Nickclothes.INC</div>
+      <div>Prueba 1</div>
+      <div className="componenten">
+        <Componenten/>
+        <Componenten/>
+        <Componenten/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Componenten.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Componenten = () => {
+  return (
+    <div>Componenten</div>
+  )
+}
+
+export default Componenten
